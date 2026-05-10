32f20783cd589080e8888c90c33926e94d273d75
paker0426 <k.hiroki.6578@gmail.com>
2026-05-10 14:58:00 +0900

    Initial Commit

---
+++ RPECalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPECalculator
+//
+//  Created by kitamurahiroki on 2026/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RPECalculator/RPECalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPECalculatorApp.swift
+//  RPECalculator
+//
+//  Created by kitamurahiroki on 2026/05/10.
+//
+
+import SwiftUI
+
+@main
+struct RPECalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
